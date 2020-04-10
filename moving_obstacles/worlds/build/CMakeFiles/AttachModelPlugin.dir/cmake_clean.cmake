cba4d19078f5b03d27ca6a99538ec4ee431edf83
chrysikos-christos <xrusikos11@gmail.com>
2020-04-10 12:11:11 +0300

    Moving humans added in Operation Room

---
+++ moving_obstacles/worlds/build/CMakeFiles/AttachModelPlugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AttachModelPlugin.dir/AttachModelPlugin.cc.o"
+  "libAttachModelPlugin.pdb"
+  "libAttachModelPlugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AttachModelPlugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ moving_obstacles/worlds/build/CMakeFiles/TrajectoryActorPlugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TrajectoryActorPlugin.dir/TrajectoryActorPlugin.cc.o"
+  "libTrajectoryActorPlugin.pdb"
+  "libTrajectoryActorPlugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TrajectoryActorPlugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
